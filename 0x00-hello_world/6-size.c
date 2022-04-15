52c88584641aebe4fb99486eb2337e8f31cb151f
Nyabonyo <nyawangasyprose@gmail.com>
2022-04-15 15:51:36 +0300

    My commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int a;
+	float b;
+	char c;
+	long int d;
+	long long int e;
+
+printf("size of int : %lu), (unsigned long)size of (a)");
+printf("size of float : %lu), (unsigned long)size of (b)");
+printf("size of char : %c), (unsigned long)size of (c)");
+printf("size of long int : %lu), (unsigned long)size of (d)");
+printf("size of long long long int : %llu), (unsigned long)size of (e)");
+
+return (0);
+}
